7a071bbdc5d05d0493c5e2ddc01ed075e8650f8c
kasidetM <kasidet.meteeputthi@thoughtworks.com>
2022-01-27 19:13:38 +0700

    Initial Commit

---
+++ Fortune/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fortune
+//
+//  Created by Kasidet Meteeputthi on 27/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fortune/FortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneApp.swift
+//  Fortune
+//
+//  Created by Kasidet Meteeputthi on 27/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
